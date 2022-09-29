710f8c9797ba9b940b1febae8bd8cf8d71633e8b
prudvi2539 <114150109+prudvi2539@users.noreply.github.com>
2022-09-29 14:33:39 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "jenkins-demo" {
+    ami = "ami-0f924dc71d44d23e2"
+    instance_type = "t2.micro"
+    count = "1"
+    key_name = "demo-key"
+
+    tags = {
+       "name" = "jenkins-demo"
+    }
+}

dc1468dea26d46a5011f7bb7ebd143ce7acf3281
prudvi2539 <114150109+prudvi2539@users.noreply.github.com>
2022-09-29 14:54:33 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "jenkins-demo" {
     ami = "ami-0f924dc71d44d23e2"
     instance_type = "t2.micro"
     count = "1"
-    key_name = "demo-key"
+    key_name = "jenkinskey"
 
     tags = {
        "name" = "jenkins-demo"
